9e55d1a8923fceb4e1b8a46be7ca4d2d7258d3c0
Brayam Programador <brateomx@gmail.com>
2025-06-05 19:01:02 -0600

    ajuste de rutas

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,25 @@ Route::get('/', function () {
     return redirect()->route($routeName);
 })->name('home');
 
+Route::middleware(['auth'])->group(function () {
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
 Route::middleware(['auth', 'verified', 'role:1,2,3,4'])->group(function () {
-    Route::view('dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
+    Route::view('dashboard', 'dashboard')->name('dashboard');
     Route::redirect('settings', 'settings/profile');
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
     Volt::route('customers', 'customers.index')->name('customers.index');
     Volt::route('users', 'users.index')->name('users.index');
 });
 
 Route::middleware(['auth', 'role:5'])->group(function () {
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-    Route::view('auditor', 'vistaAuditor')->name('vistaAuditor');
+    Route::view('vistaAuditor', 'vistaAuditor')->name('vistaAuditor');
 });
 
-Route::middleware(['auth', 'role:5'])->group(function () {
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-    Route::view('auditor6', 'vistaAuditor6')->name('vistaAuditor6');
+Route::middleware(['auth', 'role:6'])->group(function () {
+    Route::view('vistaAuditor6', 'vistaAuditor6')->name('vistaAuditor6');
 });
 
 require __DIR__.'/auth.php';
